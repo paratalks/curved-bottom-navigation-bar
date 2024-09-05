839b47da2f7addb0fbc6a0745ca81700dc682921
paratalks <164619468+paratalks@users.noreply.github.com>
2024-09-05 18:24:23 +0530

    Update RNShadow.tsx
    fixed the RNShadow issue in modern devices

--- src/RNShadow.tsx
+++ src/RNShadow.tsx
@@ -1,4 +1,3 @@
-import { Platform, requireNativeComponent, View } from 'react-native';
+import { View } from 'react-native';
 
-export const RNShadow =
-  Platform.OS === 'android' ? requireNativeComponent('RNShadow') : View;
+export const RNShadow = View;
